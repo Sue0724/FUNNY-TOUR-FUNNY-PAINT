967fbdd1b3ae2a5258cc41376de20eec99768afe
wuzhen525 <3599891046@qq.com>
2024-10-24 23:39:56 +0800

    split two modes

--- frontend/pages/map_choose_places/map_choose_places.js
+++ frontend/pages/map_choose_places/map_choose_places.js
@@ -1,31 +1,10 @@
-// pages/canvas/canvas.js
+// pages/map_choose_places/map_choose_places.js
 const app = getApp();
 
 Page({
   data: {
-    drawMode: false,   // 绘画编辑模式是否开启
-    hideDrawing: false, // 是否隐藏绘画
     showMarkers: true, // 需要保证在showMarkers=false时不放置点，可以设置为只有绘画模式下才能隐藏
     showPlaceList: false, // 是否显示地点列表
-
-    ctx: "",           // 画布上下文
-    w: 0,              // 画布宽度
-    h: 0,              // 画布高度
-    lastX: 0,          // 上一次绘画时的 X 坐标
-    lastY: 0,          // 上一次绘画时的 Y 坐标
-    paths: [],         // 用于存储所有绘制路径的数组
-    topLeft: 0,
-    latDiff: 0,
-    lngDiff: 0,
-    screenXDiff: 0,
-    screenYDiff: 0,
-    lastLatLng: {},
-    lastScreenXY: {},
-    currentTool: 'brush', // 默认工具为画笔
-    r: 0, // 画笔颜色
-    g: 0,
-    b: 0,
-    lineWidth: 5, // 画笔粗细
 
     mapCtx: "",
     latitude: 30.512015580071605, // 中心纬度
@@ -67,7 +46,7 @@ Page({
     selfAddedMarkers: [],  // 自选地点
     recommendMarkers: [],  // 推荐地点
     tripCollectedMarkers: [],  // 当前日程包含的收藏地点
-    tripPlanMarkers: [],  // 当前日程添加的地点
+    tripPlanMarkers: [],  // 当前行程添加的地点
     markers: [],
     markers_backup: [],
 
@@ -75,7 +54,7 @@ Page({
     currentMarker: null,
     actions: [
       { text: '查看详情', value: 'showDetail' },
-      { text: '加入日程', value: 'add' },
+      { text: '加入行程', value: 'add' },
       { text: '收藏地点', value: 'collect' },
       { text: '外部导航', value: 'outerNavigate' },
     ],
@@ -86,7 +65,6 @@ Page({
     const that = this;
     wx.chooseLocation({
       success(res) {
-        console.log(res);
         const {latitude, longitude, name} = res;
 
         const newMarker = {
@@ -95,7 +73,7 @@ Page({
           iconPath: '../../images/marker/center.png',
           latitude: latitude,
           longitude: longitude,
-          category: "日程中心",
+          category: "行程中心",
           distance: 0,
           width: 50,
           height: 50,
@@ -217,21 +195,14 @@ Page({
     });
   },
 
-  // 切换绘画模式
-  async toggleDrawMode() {
-    this.setData({
-      drawMode: !this.data.drawMode,
-      currentTool: 'brush' // 进入绘画模式时重置工具为画笔
+  // 跳转绘画模式
+  navigateToDrawMode() {
+    const markers = JSON.stringify(this.data.tripPlanMarkers); // 序列化 markers
+    const fixedLat = this.data.chosenFixedLatitude;
+    const fixedLng = this.data.chosenFixedLongitude;
+    wx.navigateTo({
+      url: `/pages/map_canvas/map_canvas?markers=${encodeURIComponent(markers)}&fixedLat=${fixedLat}&fixedLng=${fixedLng}`
     });
-
-    if (this.data.drawMode) {
-      const { latitude, longitude } = await this.getMapCenterLatLng();
-
-      this.setData({
-        latitude: latitude,
-        longitude: longitude
-      });
-    }
   },
 
   // 切换是否显示地点标记
@@ -252,21 +223,6 @@ Page({
         currentMarker: null,
       });
     }
-  },
-
-  // 切换是否隐藏绘画
-  toggleHideDrawing() {
-      this.setData({
-        hideDrawing: !this.data.hideDrawing,
-      })
-
-      const ctx = this.data.ctx;
-      if (this.data.hideDrawing) {
-        ctx.clearRect(0, 0, this.data.w, this.data.h);  // 清空画布
-      }
-      else {
-        this.renderPaths(ctx);
-      }
   },
 
   // 切换是否显示地点列表
@@ -312,354 +268,9 @@ Page({
     return R * c; // 距离，单位为米
   },
 
-  // 获取地图左上角经纬度
-  getTopLeftLatLng() {
-    return new Promise((resolve, reject) => {
-      const mapCtx = wx.createMapContext('map');
-      
-      mapCtx.getRegion({
-        success: (res) => {
-          const { southwest, northeast } = res;
-
-          // 计算最左上角的经纬度
-          const topLeftLat = northeast.latitude;
-          const topLeftLng = southwest.longitude;
-
-          // 返回结果
-          // console.log({ latitude: topLeftLat, longitude: topLeftLng })
-          resolve({ latitude: topLeftLat, longitude: topLeftLng });
-        },
-        fail: (err) => {
-          // 返回错误
-          reject(err);
-        }
-      });
-    });
-  },
-
-  // 获取地图中心点经纬度
-  getMapCenterLatLng() {
-    return new Promise((resolve, reject) => {
-      const mapCtx = wx.createMapContext('map');
-
-      mapCtx.getCenterLocation({
-        success: (res) => {
-          const { latitude, longitude } = res;
-
-          // 返回结果
-          // console.log({ latitude: latitude, longitude: longitude })
-          resolve({ latitude: latitude, longitude: longitude });
-        },
-        fail: (err) => {
-          // 返回错误
-          reject(err);
-        }
-      });
-    });
-  },
-
-  // 将屏幕坐标转换为经纬度
-  convertScreenToLatLng(x, y) {
-    const resLat = this.data.topLeft.latitude + y * this.data.latDiff / this.data.screenYDiff;
-    const resLng = this.data.topLeft.longitude + x * this.data.lngDiff / this.data.screenXDiff;
-
-    return { latitude: resLat, longitude: resLng }
-  },
-
-  // 将经纬度转换为屏幕坐标
-  convertLatLngToScreen(lng, lat) {
-    const resX = (lng - this.data.topLeft.longitude) / this.data.lngDiff * this.data.screenXDiff;
-    const resY = (lat - this.data.topLeft.latitude) / this.data.latDiff * this.data.screenYDiff;
-
-    return { x: resX, y: resY };
-  },
-
-  // 开始移动（按下按钮时触发）
-  startMove(direction) {
-    const mapCtx = wx.createMapContext('map');
-
-    let newLatitude = this.data.latitude;
-    let newLongitude = this.data.longitude;
-
-    // 根据方向更新经纬度
-    switch (direction) {
-      case 'up':
-          newLatitude += this.data.moveStep;
-          break;
-      case 'down':
-          newLatitude -= this.data.moveStep;
-          break;
-      case 'left':
-          newLongitude -= this.data.moveStep;
-          break;
-      case 'right':
-          newLongitude += this.data.moveStep;
-          break;
-    }
-
-    this.setData({
-        latitude: newLatitude,
-        longitude: newLongitude
-    }, () => {
-        // 移动地图到新的位置
-        mapCtx.moveToLocation({
-          latitude: this.data.latitude,
-          longitude: this.data.longitude
-        });
-    });
-  },
-
-  // 左移地图
-  moveLeft() {
-    this.startMove('left');
-  },
-
-  // 右移地图
-  moveRight() {
-    this.startMove('right');
-  },
-
-  // 上移地图
-  moveUp() {
-    this.startMove('up');
-  },
-
-  // 下移地图
-  moveDown() {
-    this.startMove('down');
-  },
-
-  onLoad: async function () {
-    // 初始化画布上下文
-    wx.createSelectorQuery()
-      .select('#myCanvas')   // 选择 Canvas 节点
-      .fields({
-        node: true,
-        size: true,
-      })
-      .exec(this.init.bind(this));
-
-    await this.setCurrentCoordinate();
-
+  onLoad: function () {
     const mapCtx = wx.createMapContext('map');
     this.setData({ mapCtx: mapCtx });
-  },
-
-  init(res) {
-    // 获取 Canvas 节点的宽度和高度（单位是 CSS 像素）
-    const width = res[0].width;
-    const height = res[0].height;
-    this.setData({ w: width, h: height });
-
-    const canvas = res[0].node;
-    const ctx = canvas.getContext('2d');
-
-    // 获取屏幕像素比，像素比表示屏幕上一个物理像素和一个 CSS 像素的比例
-    // Canvas 的尺寸在物理像素上放大了，但在逻辑上（CSS 像素）保持不变
-    const dpr = wx.getSystemInfoSync().pixelRatio;
-    canvas.width = width * dpr;
-    canvas.height = height * dpr;
-    ctx.scale(dpr, dpr);  // 根据设备像素比缩放
-
-    this.setData({ ctx: ctx });
-  },
-
-  async setCurrentCoordinate() {
-    try {
-      // 获取地图左上角和中心的经纬度
-      const [topLeft, center] = await Promise.all([this.getTopLeftLatLng(), this.getMapCenterLatLng()]);
-
-      // 计算地图中心到左上角的经纬度差异
-      const latDiff = center.latitude - topLeft.latitude;
-      const lngDiff = center.longitude - topLeft.longitude;
-
-      // 计算屏幕中心到左上角的屏幕坐标差异
-      const screenXDiff = this.data.w / 2;
-      const screenYDiff = this.data.h / 2;
-
-      // 更新数据
-      this.setData({
-        topLeft: topLeft,
-        latDiff: latDiff,
-        lngDiff: lngDiff,
-        screenXDiff: screenXDiff,
-        screenYDiff: screenYDiff,
-        moveStep: 0.3 * Math.abs(lngDiff),
-      });
-    } catch (err) {
-      console.error("Error in setCurrentCoordinate:", err);
-    };
-  },
-
-  // 地图拖动或缩放，重新绘制路径
-  async onMapMove(e) {
-    // console.log(e.type);
-    if (e.type == "begin") {
-      const ctx = this.data.ctx;
-      ctx.clearRect(0, 0, this.data.w, this.data.h);  // 清空画布
-    }
-    else if (e.type == "end") {
-      if (!this.data.hideDrawing) {
-        await this.setCurrentCoordinate();
-        this.renderPaths(this.data.ctx);
-      }
-    }
-  },
-
-  changeColor: function (e) {
-    const colorType = e.currentTarget.dataset.type;
-    this.setData({ [colorType]: e.detail.value });
-  },
-
-  changeLineWidth: function (e) {
-    this.setData({ lineWidth: e.detail.value });
-  },
-
-  // 渲染全部路径
-  renderPaths(ctx) {
-    // ctx.lineCap = "round";
-    // ctx.lineJoin = "round";
-
-    this.data.paths.forEach(path => {
-      // 将路径的起点和终点经纬度转换为屏幕坐标
-      const start = this.convertLatLngToScreen(path.startLng, path.startLat);
-      const end = this.convertLatLngToScreen(path.endLng, path.endLat);
-      ctx.strokeStyle = path.strokeStyle;
-      ctx.lineWidth = path.lineWidth;
-
-      // 绘制路径
-      ctx.beginPath();
-      ctx.moveTo(start.x, start.y);
-      ctx.lineTo(end.x, end.y);
-      ctx.stroke();
-    });
-  },
-
-  // 根据当前工具渲染新增路径
-  renderNewPath(ctx, x, y) {
-    ctx.strokeStyle = `rgb(${this.data.r}, ${this.data.g}, ${this.data.b})`; // 画笔颜色
-    ctx.lineWidth = this.data.lineWidth; // 画笔粗细
-    // ctx.lineCap = "round"; // 设置线条端点样式
-    // ctx.lineJoin = "round"; // 设置线条连接样式
-
-    // 绘制路径
-    ctx.beginPath();
-    ctx.moveTo(this.data.lastScreenXY.x, this.data.lastScreenXY.y);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-  },
-
-  // 检测并擦除接触的路径
-  erasePath(x, y) {
-    const tolerance = 10; // 碰撞检测的容忍度
-    const newPaths = this.data.paths.filter(path => {
-        const start = this.convertLatLngToScreen(path.startLng, path.startLat);
-        const end = this.convertLatLngToScreen(path.endLng, path.endLat);
-
-        // 计算线段到点的距离
-        const dist = this.pointToLineDistance({ x, y }, start, end);
-        return dist > tolerance; // 仅保留未接触的路径
-    });
-
-    this.setData({ paths: newPaths });
-    const ctx = this.data.ctx;
-    ctx.clearRect(0, 0, this.data.w, this.data.h); // 清空画布
-    this.renderPaths(this.data.ctx); // 重新渲染路径
-  },
-
-  // 计算点到线段的距离
-  pointToLineDistance(point, start, end) {
-    const A = point.x - start.x;
-    const B = point.y - start.y;
-    const C = end.x - start.x;
-    const D = end.y - start.y;
-
-    const dot = A * C + B * D;
-    const len_sq = C * C + D * D;
-    const param = len_sq !== 0 ? dot / len_sq : -1;
-
-    let xx, yy;
-
-    if (param < 0) {
-        xx = start.x;
-        yy = start.y;
-    } else if (param > 1) {
-        xx = end.x;
-        yy = end.y;
-    } else {
-        xx = start.x + param * C;
-        yy = start.y + param * D;
-    }
-
-    const dx = point.x - xx;
-    const dy = point.y - yy;
-    return Math.sqrt(dx * dx + dy * dy); // 返回距离
-  },
-
-  // 用户触摸画布开始时触发（开始绘制）
-  touchStart: function (e) {
-    const x = e.touches[0].x;
-    const y = e.touches[0].y;
-
-    // 将屏幕坐标转换为经纬度并保存
-    const latLng = this.convertScreenToLatLng(x, y);
-    this.setData({
-      lastLatLng: latLng,  // 保存起点的经纬度
-      lastScreenXY: {x: x, y: y}
-    });
-  },
-
-  // 用户在画布上滑动手指时触发（绘制或擦除过程）
-  touchMove: function (e) {
-    const x = e.touches[0].x;  // 获取当前触摸点的 X 坐标
-    const y = e.touches[0].y;  // 获取当前触摸点的 Y 坐标
-
-    if (this.data.currentTool === 'eraser') {
-      this.erasePath(x, y);
-    } else {
-      // 渲染新增绘制路径
-      this.renderNewPath(this.data.ctx, x, y);
-
-      const latLng = this.convertScreenToLatLng(x, y);
-      const newPath = {
-        startLat: this.data.lastLatLng.latitude,
-        startLng: this.data.lastLatLng.longitude,
-        endLat: latLng.latitude,
-        endLng: latLng.longitude,
-        strokeStyle: `rgb(${this.data.r}, ${this.data.g}, ${this.data.b})`,
-        lineWidth: this.data.lineWidth
-      };
-
-      // 保存路径并更新
-      this.data.paths.push(newPath); // 直接推入数组，减少setData调用
-      this.setData({
-        lastLatLng: latLng,
-        lastScreenXY: {x: x, y: y}
-      });
-    }
-  },
-
-  // 用户触摸结束时触发（结束绘制）
-  touchEnd: function () {
-
-  },
-
-  // 清空画布
-  clearCanvas() {
-    this.setData({ currentTool: 'clear' });
-    const ctx = this.data.ctx;
-    ctx.clearRect(0, 0, this.data.w, this.data.h); // 清空画布
-    this.setData({ paths: [] }); // 清空路径记录
-  },
-
-  // 选择画笔工具
-  selectBrush() {
-    this.setData({ currentTool: 'brush' });
-  },
-
-  // 选择橡皮擦工具
-  selectEraser() {
-    this.setData({ currentTool: 'eraser' });
   },
 
   /**
@@ -992,7 +603,7 @@ Page({
     });
   },
 
-  // 加入日程
+  // 加入行程
   addPlace() {
     this.setData({
       tripPlanMarkers: [this.data.currentMarker].concat(this.data.tripPlanMarkers)
@@ -1014,7 +625,7 @@ Page({
   showDetail() {
     const markerName = this.data.currentMarker.name;
     wx.navigateTo({
-      url: `/pages/canvas/place_detail/place_detail?name=${encodeURIComponent(markerName)}` // 构建跳转 URL，并编码参数
+      url: `/pages/map_choose_places/place_detail/place_detail?name=${encodeURIComponent(markerName)}` // 构建跳转 URL，并编码参数
     });
   },
 
